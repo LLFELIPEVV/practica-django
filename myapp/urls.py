994268695ed655674f292d7894d9b7f8b0949c70
FELIPE <felipe3133770868@gmail.com>
2023-07-11 15:03:05 -0500

    URL names

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import hello, about, index, projects, tasks, create_task, create_project
 
 urlpatterns = [
-    path('', index),
-    path('about/', about),
-    path('hello/<str:username>', hello),
-    path('projects/', projects),
-    path('tasks/', tasks),
-    path('create_task/', create_task),
-    path('create_project', create_project),
+    path('', index, name = "index"),
+    path('about/', about, name = "about"),
+    path('hello/<str:username>', hello, name = "hello"),
+    path('projects/', projects, name = "projects"),
+    path('tasks/', tasks, name = "tasks"),
+    path('create_task/', create_task, name = "create_task"),
+    path('create_project/', create_project, name = "create_project"),
 ]
