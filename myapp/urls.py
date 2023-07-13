49e27947f14120c949f107e85ff3b14a76a5005a
FELIPE <felipe3133770868@gmail.com>
2023-07-13 16:02:18 -0500

    Mejoras finales

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import hello, about, index, projects, tasks, create_task, create_project
+from .views import hello, about, index, projects, tasks, create_task, create_project, project_detail
 
 urlpatterns = [
     path('', index, name = "index"),
     path('about/', about, name = "about"),
     path('hello/<str:username>', hello, name = "hello"),
     path('projects/', projects, name = "projects"),
+    path('projects/<int:id>', project_detail, name="project_detail"),
     path('tasks/', tasks, name = "tasks"),
     path('create_task/', create_task, name = "create_task"),
     path('create_project/', create_project, name = "create_project"),
